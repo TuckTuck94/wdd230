d10625f2b16adb070ccd9ee567fae4b0d53c7c08
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2023-09-21 10:01:18 -0500

    fixing base.css errors

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //year of copyright
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
@@ -13,19 +12,3 @@ else {
     document.getElementById("modified").innerHTML = "Last Modified: " +
         temp + " am"
 }
-=======
-//year of copyright
-document.getElementById("year").innerHTML = new Date().getFullYear();
-
-//last modified
-var temp = document.lastModified
-var n = parseInt(temp.substring(temp.length-8,temp.length-6))
-if (n>12) {
-    document.getElementById("modified").innerHTML = "Last Modified: "+
-        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
-}
-else {
-    document.getElementById("modified").innerHTML = "Last Modified: "+
-        temp+" am"
-}
->>>>>>> 1b8563b73f62b94621914ec86ec4b00f48272d33
