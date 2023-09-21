234f190153a45e468ab7d44f1f7cbcc74eb226d7
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2023-09-21 11:05:07 -0500

    finished design principles.

---
+++ week02/scripts/getdates.js
@@ -0,0 +1,14 @@
+//year of copyright
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+//last modified
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length - 8, temp.length - 6))
+if (n > 12) {
+    document.getElementById("modified").innerHTML = "Last Modified: " +
+        temp.substring(0, temp.length - 8) + (n - 12) + temp.substring(temp.length - 6) + " pm"
+}
+else {
+    document.getElementById("modified").innerHTML = "Last Modified: " +
+        temp + " am"
+}
