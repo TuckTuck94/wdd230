6b1b9d7fbd51d60788414e85ebbdf501743c8d54
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2023-10-21 18:54:18 -0500

    hero img and weather scripts

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -29,3 +29,32 @@ modeButton.addEventListener("click", () => {
         modeButton.textContent = "🕶️";
     }
 });
+
+/*WEATHER*/
+!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js');
+/*WIND CHILL*/
+const tempInput = document.getElementById('temperature');
+tempInput.addEventListener("keyup", getUpdate);
+
+const wspeedInput = document.getElementById('windspeed');
+wspeedInput.addEventListener("keyup", getUpdate);
+
+getUpdate();
+
+function getUpdate() {
+    const temp = parseInt(document.getElementById('temperature').value);
+    const wspeed = parseFloat(document.getElementById('windspeed').value);
+
+    if (temp <= 50 && wspeed > 3.0) {
+        const windChill = calculateWindChill(temp, wspeed)
+        document.getElementById('windchill').textContent = windChill.toFixed(2) + " °F";
+    } else {
+
+        document.getElementById('windchill').textContent = "n/a";
+    }
+}
+
+function calculateWindChill(temp, wspeed) {
+    const windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wspeed, 0.16) + 0.4275 * temp * Math.pow(wspeed, 0.16);
+    return windChill;
+}
