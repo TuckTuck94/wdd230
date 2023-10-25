74583d0a9732160a4aaff45377209f8efcf845cd
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2023-10-25 17:30:28 -0500

    added visits javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,16 @@ const options = { year: 'numeric' };
 document.getElementById("currentdate").innerHTML = new Date().getFullYear()
 
 document.getElementById('lastModified').innerHTML = new Date(document.lastModified);
+
+//Number of Visits
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}
+else {
+    visitsDisplay.textContent = "Welcome to my Website!";
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
+}
