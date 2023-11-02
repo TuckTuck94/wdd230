6fb330c6979691980d966134ae3d33aefd504c2f
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2023-11-02 18:31:11 -0500

    form assignment

---
+++ scripts/rangeslider.js
@@ -0,0 +1,19 @@
+const value = document.querySelector("#ratingText");
+const range = document.querySelector("#rating");
+
+// RANGE event listener
+range.addEventListener('change', displayValue);
+range.addEventListener('input', displayValue);
+
+function displayValue() {
+    let stars = "";
+    for (let i = 0; i < range.value; i++) {
+        stars = stars + '★';
+    }
+    for (let i = 0; i < (10 - range.value); i++) {
+        stars = stars + '☆';
+    }
+    value.innerText = `Page Rating: ${stars} (${range.value} out of 10)`;
+}
+
+value.innerText = "Page Rating: ★★★★★☆☆☆☆☆ (5 out of 10)";
