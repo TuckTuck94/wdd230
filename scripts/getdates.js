79ecee3ff0f1b30b24fbe8cbed00b85d9a3220b4
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2023-11-16 14:03:18 -0600

    weather API. Not working. Frustrating

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -20,3 +20,41 @@ else {
 
 // Store the number of visits in the user's browser.
 localStorage.setItem("numVisits-ls", numVisits);
+
+// Weather
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const weatherDesc = document.querySelector('#weather-desc');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=-97&appid=e935a5a6a1ec7a93d72d1fa9eceab7a2';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayWeather(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayWeather(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;C`;
+    data.weather.forEach((event) => {
+        const iconsrc = `https://openweathermap.org/img/wn/${event.icon}.png`;
+        let desc = event.description;
+
+        weatherIcon.setAttribute('src', iconsrc);
+        weatherIcon.setAttribute('alt', `weather icon ${event.description}`);
+        weatherDesc.textContent = `${desc}`;
+        weatherDesc.style.textTransform = 'capitalize';
+    })
+}
